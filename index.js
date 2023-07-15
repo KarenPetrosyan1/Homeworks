90b5b91580ef8690e9d2b7ccdb33bc5e0c06c54d
KarenPetrosyan1 <138508157+KarenPetrosyan1@users.noreply.github.com>
2023-07-15 19:18:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+let oddEven = function(){
+const odd = []
+const even = [] 
+for(let i = 0; i < oddEven.length; i++){
+    if(oddEven[i] % 2 !== 0){
+    odd.push(oddEven[i])
+      } else {
+      even.push(oddEven[i]) 
+        console.log(odd, even)
+    }
+  }
+}  oddEven([45, 12, 3, 6, 17, 0])
+
+
+// Task1.
+   
+// const arr = [45, 12, 3, 6, 17, 0]
+// const odd = []
+// const even = [] 
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 !== 0){
+//     odd.push(arr[i])
+//     } else {
+//       even.push(arr[i])
+//   }
+// }
+//    console.log(odd, even)
+
+
+//Task2.
+
+// function arrays(){
+//   let array = []
+//   for(let i = 0; i < array.length; i++){
+//     let oper = ""
+//     switch(oper){
+//         case("+"){
+//           array[i] += array[i+1]
+//         }
+//         case("-"){
+//           array[i] += array[i+1]
+//         }
+//         case("*"){
+//           array[i] += array[i+1]
+//         }
+//         case("/"){
+//           array[i] += array[i+1]
+//         }
+        
+      
+//     }
+
+    
+//     function Sum() {}    return 
+//     }
+//   }
+    
+// }
